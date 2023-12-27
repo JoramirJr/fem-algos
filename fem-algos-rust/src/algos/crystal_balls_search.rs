98c7b89cbe2a8395aadc662042fef2c6484b6b51
Joramir da Silva Jr <joramirjr@outlook.com>
2023-12-27 12:21:01 -0300

    two ball problem (rs) lacking tests

--- fem-algos-rust/src/algos/crystal_balls_search.rs
+++ fem-algos-rust/src/algos/crystal_balls_search.rs
@@ -1,18 +1,65 @@
 use num::integer::Roots;
 
-pub fn crystal_balls_search(container: [usize; 20], search: usize) {
-    let leng_sqrt = container.len();
-    leng_sqrt.sqrt();
-    let mut while_index = leng_sqrt;
+pub fn crystal_balls_search(container: [usize; 20], needle: usize) -> Option<usize> {
+    let leng_sqr = container.len();
+    leng_sqr.sqrt();
+    let mut sqr_loop_idx = leng_sqr;
 
-    let first_loop_rst = loop {
-        if container[while_index] == search {
-            break container[while_index];
+    let breakarea_search = loop {
+        if sqr_loop_idx < leng_sqr {
+            sqr_loop_idx += leng_sqr;
+            if container[sqr_loop_idx] == needle {
+                break Some(container[sqr_loop_idx]);
+            }
+        } else {
+            sqr_loop_idx += leng_sqr;
+            break None;
+        }
+    };
+
+    if breakarea_search.is_some() {
+        return Some(container[sqr_loop_idx]);
+    }
+
+    sqr_loop_idx -= leng_sqr;
+    let mut linear_sqr_idx = sqr_loop_idx;
+
+    loop {
+        if linear_sqr_idx > sqr_loop_idx {
+            break None;
+        } else {
+            if container[linear_sqr_idx] == needle {
+                break Some(container[linear_sqr_idx]);
+            }
+        }
+        linear_sqr_idx += 1;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::crystal_balls_search;
+    use rand::{rngs::ThreadRng, Rng};
+    use std::iter::repeat;
+
+    #[test]
+    fn basics() {
+        let mut rng: ThreadRng = rand::thread_rng();
+        let idx = rng.gen_range(0..10_000);
+
+        let thousand_falses = repeat(false).take(1000);
+
+        let mut data = Vec::from_iter(thousand_falses);
+
+        for i in idx {
+            data[i] = true;
         }
 
-    }
-    if first_loop_rst {
-        return fir
-    }
+        // for (let i = idx; i < 10000; ++i) {
+        //     data[i] = true;
+        // }
 
+        // expect(two_crystal_balls(data)).toEqual(idx);
+        // expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
+    }
 }
